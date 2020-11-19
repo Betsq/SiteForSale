105562ed4b15d88befeb31b7047c12352456113c
Betsq <betsq9@gmail.com>
2020-11-19 12:00:57 +0200

    Добавьте файлы проекта.

---
+++ BeautySouthKoreaSiteMVC/Models/Cosmetic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautySouthKoreaSiteMVC.Models
+{
+    public class Cosmetic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Price { get; set; }
+        public string PurposeFor { get; set; } //Преназначение для (Глаз, губ и т.д.)
+        public string Means { get; set; } //Средство (тональный крем, тени, блески и т.д.) 
+
+
+    }
+}
